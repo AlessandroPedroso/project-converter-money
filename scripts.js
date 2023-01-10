31c0f097ec7678c39738f4cbdf96a68d0919881b
AlessandroPedroso <alessandropedrosoti@gmail.com>
2023-01-10 16:04:50 -0300

    conclusão do projeto

--- scripts.js
+++ scripts.js
@@ -1,12 +1,60 @@
-const button  = document.getElementById('convert-button')
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currencry-select')
 
-const dolar = 5.2
+const dolar = 5.22
+const euro = 5.60
+const bitcoin = 90.89213
 
-const convertValues = () =>{
+const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const dorlarValueText = document.getElementById('doral-value-text')
 
-    console.log(inputReais / dolar)
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReais)
 
+    if (select.value === "US$ Dólar americano") {
+        dorlarValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar) // '$100.00'
+    }
+
+    if (select.value === "€ Euro") {
+        dorlarValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReais / euro)
+    }
+
+    if(select.value === "Bitcoin"){
+        dorlarValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'BTC' }
+        ).format(inputReais / bitcoin)
+    }
+}
+
+const changeCurrency = () => {
+
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === "US$ Dólar americano") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./assets/eua.png"
+    }
+
+    if (select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/euro.jpg"
+    }
+
+    if(select.value === "Bitcoin"){
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoins.png"
+    }
+
+    convertValues()
 }
 
 button.addEventListener('click', convertValues)
+select.addEventListener("change", changeCurrency)
