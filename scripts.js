7d185fe4eb695af886f459316fe70a6dc460c562
AlessandroPedroso <alessandropedrosoti@gmail.com>
2023-02-09 11:38:49 -0300

    pegando de forma dinâmica pela api

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currencry-select')
 
-const dolar = 5.22
-const euro = 5.60
-const bitcoin = 90.89213
+// const dolar = 5.22
+// const euro = 5.60
+// const bitcoin = 90.89213
 
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const dorlarValueText = document.getElementById('doral-value-text')
+
+    const data = await fetch('http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL').then(response => response.json());
+    // const recebeDados = await data.json();
+
+    const dolar = data.USDBRL.high;
+    const euro = data.EURBRL.high;
+    const bitcoin = data.BTCBRL.bid;
+
+    // console.log(dolar);
+    // console.log(euro);
+    // console.log(data);
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
